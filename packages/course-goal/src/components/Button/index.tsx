66dd801aa97b44a094ce5de12332f8d5e7ac3750
harry <shsage91@gmail.com>
2021-05-29 23:47:18 +0900

    # package/course-goal [MOD] - css module & styled-components

--- packages/course-goal/src/components/Button/index.tsx
+++ packages/course-goal/src/components/Button/index.tsx
@@ -1,16 +1,43 @@
-import React, { FC, MouseEvent } from "react";
-import "./Button.css";
+import React, { FC } from "react";
+import styled from "styled-components";
+import classes from "./Button.module.css";
+
+
+// export const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover, &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+//
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+// `
 
 
 interface Props {
   type?: "submit" | "reset" | "button";
-  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e: React.MouseEvent) => void;
 }
 
-export const Button: FC<Props> = ({ type, onClick, children }) => {
+export const Button: FC<Props> = ({ children, type, onClick }) => {
   return (
-    <button type={type} className="button" onClick={onClick}>
+    <button type={type} className={classes.button} onClick={onClick}>
       { children }
     </button>
-  );
-};
+  )
+}
--- packages/course-goal/src/components/CourseInput/index.tsx
+++ packages/course-goal/src/components/CourseInput/index.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, FC, FormEvent, useState } from "react";
 import { Button } from "../Button";
-import "./CourseInput.css";
+import styled from "styled-components";
 
 
 interface Props {
@@ -9,23 +9,63 @@ interface Props {
 
 export const CourseInput: FC<Props> = ({ onAddGoal }) => {
   const [ enteredValue, setEnteredValue ] = useState<string>("")
+  const [ isValid, setValid ] = useState<boolean>(true)
 
   const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setEnteredValue(event.target.value)
+    const { value } = event.target
+    setValid(value.trim().length !== 0)
+    setEnteredValue(value)
   }
 
   const onFormSubmit = (event: FormEvent) => {
     event.preventDefault()
+    if (enteredValue.trim().length === 0) {
+      setValid(false)
+      return
+    }
     onAddGoal(enteredValue)
+    setValid(true)
   }
 
   return (
     <form onSubmit={onFormSubmit}>
-      <div className="form-control">
+      <FormControl isValid={isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={onInputChange}/>
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
+
+
+interface FormControlProps {
+  isValid?: boolean;
+}
+
+const FormControl = styled.div<FormControlProps>`
+  margin: 0.5rem 0;
+  
+  label {
+    color: ${props => props.isValid ? "#000" : "red"};
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+  
+  input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => props.isValid ? "#ccc" : "red"};
+    background-color: ${props => props.isValid ? "#fff" : "#ffd7d7"};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+  
+  input:focus {
+    outline: none;
+    background-color: #fad0ec;
+    border-color: #8b005d;
+  }
+`
